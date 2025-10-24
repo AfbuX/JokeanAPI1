4a6361ca9c0f3df3984fb828edf6a8efd4c38923
AfbuX <andres.betancur@correo.tdea.edu.co>
2025-10-23 19:14:24 -0500

    Cambios de Metodo de Pago

---
+++ JokeanAPI1Repository/Implements/MetododePagoQueries.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using JokeanAPI1Models;
+using JokeanAPI1Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokeanAPI1Repository.Implements
+{
+    public class MetododePagoQueries : IMetododePagoQueries
+    {
+        private readonly IDbConnection _db;
+        public MetododePagoQueries(IDbConnection db)
+        {
+
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+
+        }
+
+        public async Task<IEnumerable<MetodoPago>> GetAll()
+        {
+            try
+            {
+                string sql = "SELECT * FROM MetodoPago;";
+                var rs = await _db.QueryAsync<MetodoPago>(sql);
+                return rs;
+            }
+            catch (Exception) { throw; }
+        }
+    }
+}
---
+++ JokeanAPI1Repository/Interfaces/IPagoRepository.cs
@@ -0,0 +1,14 @@
+﻿using JokeanAPI1Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokeanAPI1Repository.Interfaces
+{
+    public interface IPagoRepository
+    {
+        Task<Pago> Add(Pago pago);
+    }
+}
